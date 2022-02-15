7be277c99bb363cd9a458873e8c7c1949b5a4cc1
Guenter Senyurt <senyurtguenter@gmail.com>
2022-02-15 18:47:46 +0100

    deneme 15.02.2021

--- src/test/java/tests/US_0002/TestCase01.java
+++ src/test/java/tests/US_0002/TestCase01.java
@@ -1,13 +1,26 @@
 package tests.US_0002;
 
-import org.openqa.selenium.WebDriver;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import pages.HotelMyCampMain;
 
 public class TestCase01 {
 
+    //Anasayfadaki sag üstteki navigasyon barinda "Log in" butonu görülebilmelidir-AC1
 
+    HotelMyCampMain loginpage;
+
+    @Test
+    public void seeIfLogIn(){
+
+        loginpage=new HotelMyCampMain();
+       // boolean actualLoginIfDisplayed = loginpage.logInButon.isDisplayed();
+       // Assert.assertTrue(actualLoginIfDisplayed);
+
+        //String currentURL = driver.getCurrentUrl();
+        //System.out.println(currentURL);
+        //Assert.assertEquals(currentURL, "https://www.hotelmycamp.com/");
+    }
 
     }
 
